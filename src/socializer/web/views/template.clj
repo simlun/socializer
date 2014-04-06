b0693a7e7a38095ba9367a5e07012af807044429
Simon Lundmark <simon.lundmark@gmail.com>
2014-04-06 18:15:17 +0200

    Sort menu items

--- src/socializer/web/views/template.clj
+++ src/socializer/web/views/template.clj
@@ -11,14 +11,14 @@
   [(keyword (str "#nav-" (:active-nav context)))] (html/add-class "active")
 
   [:.groups :.group]
-    (html/clone-for [[group-name group] (:groups (:session context))]
+    (html/clone-for [[group-name group] (into (sorted-map) (:groups (:session context)))]
                     [:a] (html/set-attr :href (str "/groups/"
                                                    (url-encode group-name)))
                     [:.name] (html/content group-name)
                     [:.count] (html/content (str (count (:members group)))))
 
   [:.events :.event]
-    (html/clone-for [[event-key event] (:events (:session context))]
+    (html/clone-for [[event-key event] (into (sorted-map) (:events (:session context)))]
                     [:a] (html/set-attr :href (str "/events/"
                                                    (url-encode event-key)))
                     [:.name] (html/content event-key)
